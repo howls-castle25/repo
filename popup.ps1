e0190d5623b783129ae66df42fed5351a99348fa
howls-castle25 <howlmoving2025@gmail.com>
2026-01-21 20:43:03 +0800

    Update popup.ps1

--- popup.ps1
+++ popup.ps1
@@ -1,6 +1,7 @@
+[Console]::Beep(1000, 500)
+
 $Time = Get-Date
 "Success! Payload executed as $env:USERNAME at $Time" | Out-File "C:\Users\Public\proof.txt" -Force
-[Console]::Beep(1000, 500)
 
 # Add-Type -AssemblyName PresentationFramework
 # [System.Windows.MessageBox]::Show("I'm working", "Status")
