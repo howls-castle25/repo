266c800c95f0dff85e0a4382f658b08bb0eab608
howls-castle25 <howlmoving2025@gmail.com>
2026-01-21 20:40:44 +0800

    Update popup.ps1

--- popup.ps1
+++ popup.ps1
@@ -1,7 +1,7 @@
-[Console]::Beep(1000, 500) # Frequency 1000Hz, Duration 500ms
-
 $Time = Get-Date
 "Success! Payload executed as $env:USERNAME at $Time" | Out-File "C:\Users\Public\proof.txt" -Force
+[Console]::Beep(1000, 500)
 
-Add-Type -AssemblyName PresentationFramework
-[System.Windows.MessageBox]::Show("I'm working", "Status")
+# Add-Type -AssemblyName PresentationFramework
+# [System.Windows.MessageBox]::Show("I'm working", "Status")
+
