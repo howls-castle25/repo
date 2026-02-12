5a6504526bebe073c5fa7e896c7a81c19dec0bb2
howls-castle25 <howlmoving2025@gmail.com>
2026-02-13 01:23:06 +0800

    Update popup.ps1

--- popup.ps1
+++ popup.ps1
@@ -2,8 +2,11 @@
 # [System.Windows.MessageBox]::Show("I'm working", "Status")
 
 
-$Time = Get-Date
-"Success! Payload executed as $env:USERNAME at $Time" | Out-File "C:\Users\Public\proof.txt" -Force
+# $Time = Get-Date
+# "Success! Payload executed as $env:USERNAME at $Time" | Out-File "C:\Users\Public\proof.txt" -Force
+
+New-Item C:\example.txt
+[System.Windows.Forms.MessageBox]::Show("Hello World")
 
 [Console]::Beep(1000, 500) # Frequency 1000Hz, Duration 500ms
 
